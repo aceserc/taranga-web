123770ec25e07e7d7f46bbec0e7426159a5ef123
jrtilak <tilakthapa9898@gmail.com>
2025-10-12 15:13:01 +0545

    refactor(techfests): improve code formatting and component exports

--- src/app/(techfests)/_components/header.tsx
+++ src/app/(techfests)/_components/header.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 
-import { useParams } from "next/navigation"
-import { Header as H } from "@/components/globals/header"
-import Link from "next/link"
-import { cn } from "@/helpers/cn"
-import { buttonVariants } from "@/components/ui/button"
-import { ArrowRightIcon } from "lucide-react"
+import { useParams } from "next/navigation";
+import { Header as H } from "@/components/globals/header";
+import Link from "next/link";
+import { cn } from "@/helpers/cn";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
 
 const Header = () => {
-  const { techfestVersion } = useParams() as { techfestVersion: string }
+  const { techfestVersion } = useParams() as { techfestVersion: string };
   return (
     <H
       routes={[
@@ -22,11 +22,18 @@ const Header = () => {
         },
       ]}
       rightContent={
-        <Link href={`/${techfestVersion}/main-events`} className={cn(buttonVariants({ variant: "secondary" }), "min-w-32 group/arrow max-lg:w-full")} >
-          Main Events  <ArrowRightIcon className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+        <Link
+          href={`/${techfestVersion}/main-events`}
+          className={cn(
+            buttonVariants({ variant: "secondary" }),
+            "min-w-32 group/arrow max-lg:w-full"
+          )}
+        >
+          Main Events{" "}
+          <ArrowRightIcon className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
         </Link>
       }
     />
-  )
-}
-export default Header
+  );
+};
+export { Header };
